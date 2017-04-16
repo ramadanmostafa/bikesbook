1a7a9ece978cde0bca84bb159389e2946680717b
Ramadan K.Mostafa <ramadankhalifamostafa@gmail.com>
2017-04-16 11:21:14 +0200

    update dev settings

--- bikesbook/settings/production.py
+++ bikesbook/settings/production.py
@@ -9,11 +9,17 @@ DEBUG = False
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'bikesbook',
+        # 'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
+        # 'PORT': '',
+        # 'USER': 'bikesbook',
+        # 'PASSWORD': '7JuNWzGXfy',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bikesbook',
+        'NAME': 'c4bikesbook_db',
         'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
         'PORT': '',
-        'USER': 'bikesbook',
-        'PASSWORD': '7JuNWzGXfy',
+        'USER': 'c4bikesbook',
+        'PASSWORD': 'qhMkG2K#8',
     }
 }
