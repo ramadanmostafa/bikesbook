b0a5adc1e86ab5993a44118fe35fd716cd133f36
Ramadan K.Mostafa <ramadankhalifamostafa@gmail.com>
2017-04-16 12:48:36 +0200

    update wsgi file

--- bikesbook/wsgi.py
+++ bikesbook/wsgi.py
@@ -6,12 +6,27 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
+#
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bikesbook.settings.production")
+#
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+#
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bikesbook.settings.production")
+import sys
+
+path = '/var/www/bikesbook.bike/web/bikesbook'
+if path not in sys.path:
+    sys.path.append(path)
+path += 'bikesbook'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bikesbook.settings.production'
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
